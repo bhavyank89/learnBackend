3d68facad5a6405cec547b5cc41cbf169a45549f
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-28 01:22:58 +0530

    chaining the method for a single route

--- express-01/routes/books.js
+++ express-01/routes/books.js
@@ -7,17 +7,31 @@ router.get('/', (req, res) => {
 })
 
 //Different curd operation on author/:id
-router.get('/author/:id', (req, res) => {
-    let id = req.params.id;
-    res.send(`Get author id : ${id}`);
-})
-router.put('/author/:id', (req, res) => {
-    let id = req.params.id;
-    res.send(`update author id : ${id}`);
-})
-router.delete('/author/:id', (req, res) => {
-    let id = req.params.id;
-    res.send(`delete author id : ${id}`);
-})
+// router.get('/author/:id', (req, res) => {
+//     let id = req.params.id;
+//     res.send(`Get author id : ${id}`);
+// })
+// router.put('/author/:id', (req, res) => {
+//     let id = req.params.id;
+//     res.send(`update author id : ${id}`);
+// })
+// router.delete('/author/:id', (req, res) => {
+//     let id = req.params.id;
+//     res.send(`delete author id : ${id}`);
+// })
+
+//insteed of initialising the route('/author/:id') in every method (get,put,delete)
+//we chain the method to a signle route as
+router.route('/author/:id')
+    .get((req, res) => {
+        let id = req.params.id;
+        res.send(`Get author id : ${id}`);
+    }).put((req, res) => {
+        let id = req.params.id;
+        res.send(`update author id : ${id}`);
+    }).delete((req, res) => {
+        let id = req.params.id;
+        res.send(`delete author id : ${id}`);
+    })
 
 module.exports = router;
