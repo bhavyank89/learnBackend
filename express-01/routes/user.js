60a36c9dab92d9df8ed82f51bae648c0a5333ddc
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-27 23:57:47 +0530

    add dynamic routing in user.js

--- express-01/routes/user.js
+++ express-01/routes/user.js
@@ -3,12 +3,20 @@ const router = express.Router();
 //as in our index.js, we set up our server as app
 //here we set up our server as router
 
+//Which ever get route is higher up in the code will be fulfilled first
+//As in this one first '/' --> '/news' --> '/12'
+
 router.get('/', (req, res) => { //here router path '/' --> represents localhost:3000/user/
     res.send('user list');
 })
 
 router.get('/new', (req, res) => { //here router path '/new' --> represents localhost:3000/user/new
     res.send('new user form');
+})
+
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`Get user id : ${id}`)
 })
 
 module.exports = router;
