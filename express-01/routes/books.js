2c63f6cf09bcd1c21c4edc2903088c7dde2419e2
bhavyank89 <bhavyanksarolia@gmail.com>
2024-06-28 01:18:42 +0530

    add get | put | delete method to  books route

--- express-01/routes/books.js
+++ express-01/routes/books.js
@@ -6,4 +6,18 @@ router.get('/', (req, res) => {
     res.send("List of books");
 })
 
+//Different curd operation on author/:id
+router.get('/author/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`Get author id : ${id}`);
+})
+router.put('/author/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`update author id : ${id}`);
+})
+router.delete('/author/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`delete author id : ${id}`);
+})
+
 module.exports = router;
