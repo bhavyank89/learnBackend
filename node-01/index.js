4c726d094fcad4aa5093446216edd99fef56e4fd
bhavyank89 <bhavyanksarolia@gmail.com>
2024-07-06 18:55:35 +0530

    write file

---
+++ node-01/index.js
@@ -0,0 +1,19 @@
+//requiring fs module
+const fs = require('fs');
+
+// fs module allows you to perform operations such as reading, writing, updating, and deleting files and directories. It can be used both synchronously and asynchronously.
+
+//Syncronously means it stop the further execution of the code until the fs operation is performed
+//Asyncronously means it stacks the operation and and executes the code and completes the operation in the backend.
+
+//writingFile
+//both syncronous and asyncronous way replacess the file if already exists
+console.log("start-1")
+fs.writeFileSync("write1.txt", "Entering write1.txt")
+console.log("end-1")
+
+console.log("start-2")
+fs.writeFile("write2.txt", "Entering write1.txt", () => {
+    console.log("write2.txt created succssfully!!")
+})
+console.log("end-2")
